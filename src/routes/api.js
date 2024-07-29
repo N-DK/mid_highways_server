5294b4c4bf62c0ad27e8c0ccf15b632d2baa76f5
N_DK <nguyenkhoadang135759@gmail.com>
2024-07-29 14:53:23 +0700

    change route name check-way

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const api = require('../app/controllers/APIController');
 
-router.get('/highways', api.getHighways);
+router.get('/check-way', api.getHighways);
 router.post('/highways', api.insertHighway);
 router.post('/trunk', api.insertTrunk);
 router.get('/highways/pull', api.pullHighways);
